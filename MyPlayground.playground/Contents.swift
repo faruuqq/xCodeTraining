7b0e9be94f38d0642a9ec2224abb638934217d74
Muhammad Faruuq Qayyum <faruuq@Muhammads-MacBook-Pro.local>
2019-04-09 13:42:45 +0700

    add encalsulation

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,45 @@
 import UIKit
 
-var str = "Hello, playground"
+//var str = "Hello, playground"
+
+//let name: String = "Muhsin"
+//var money: Int = 1000
+
+
+
+//let totalMoney = "\(money) " + name
+
+
+class Pikachu {
+    private var strength: Int = 0 //encapsulation
+    var heathPoint: Int = 0
+    var defense: Int = 0
+    
+    func attack(){
+        print("attack")
+    }
+    
+    func isDead() {
+        print("Game Over")
+    }
+    
+}
+
+class Pikampret {
+    var strength: Int = 0
+    var heathPoint: Int = 0
+    var defense: Int = 0
+    
+    func attack() {
+        print("attack")
+    }
+    
+    func isDead() {
+        print("Game Over")
+    }
+    
+}
+
+let player1 = Pikachu()
+player1.attack()
+
